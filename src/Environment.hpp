bf4406aed1f636129bbcb61aa1b13242c0f9786b
hashirniazi <hashirkhanniazii@gmail.com>
2026-04-22 00:18:18 +0500

    Created Environment.hpp Our main class for handling variable memory Follows a hash map with helpers to check variable name availability

---
+++ src/Environment.hpp
@@ -0,0 +1,32 @@
+#ifndef ENVIRONMENT_HPP
+#define ENVIRONMENT_HPP
+
+#include <unordered_map>
+#include <string>
+#include <any>
+#include "Token.hpp"
+#include "RuntimeError.hpp"
+
+class Environment {
+private:
+    std::unordered_map<std::string, std::any> values;
+
+public:
+    // Adds a new variable to the map
+    void define(const std::string& name, std::any value) {
+        values[name] = std::move(value);
+    }
+
+    // Looks up a variable by its Token name
+    std::any get(const Token& name) {
+        auto elem = values.find(name.lexeme);
+        if (elem != values.end()) {
+            return elem->second;
+        }
+
+        // If the variable doesn't exist, we crash the program!
+        throw RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+    }
+};
+
+#endif
